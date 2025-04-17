cd240a21393d7a25be0a0d86ade58bac5be61916
SouthNova <jsn950509@gmail.com>
2025-04-18 03:47:26 +0900

    chore: Babel 및 TypeScript 설정 업데이트, 모듈 경로 별칭 추가

--- babel.config.js
+++ babel.config.js
@@ -1,3 +1,30 @@
 module.exports = {
   presets: ['module:@react-native/babel-preset'],
+  plugins: [
+    [
+      'module-resolver',
+      {
+        root: ['.'],
+        extensions: [
+          '.ios.ts',
+          '.android.ts',
+          '.ts',
+          '.ios.tsx',
+          '.android.tsx',
+          '.tsx',
+          '.jsx',
+          '.js',
+          '.json'
+        ],
+        alias: {
+          '@': './src',
+          '@_app': './src/app',
+          '@_screens': './src/screens',
+          '@_widgets': './src/widgets',
+          '@_entities': './src/entities',
+          '@_shared': './src/shared'
+        }
+      }
+    ]
+  ]
 };
