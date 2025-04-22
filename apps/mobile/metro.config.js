300b9b814a98797888e0bec1599a76d585601784
SouthNova <jsn950509@gmail.com>
2025-04-22 21:40:37 +0900

    chore(mobile): NativeWind 파일 추가 및 구성 변경

--- apps/mobile/metro.config.js
+++ apps/mobile/metro.config.js
@@ -1,11 +1,24 @@
-const {getDefaultConfig, mergeConfig} = require('@react-native/metro-config');
+const { withNativeWind } = require('nativewind/metro');
+const { getDefaultConfig } = require('@react-native/metro-config');
+const { makeMetroConfig } = require('@rnx-kit/metro-config');
+const path = require('path'); // 이 줄을 추가
+
+const defaultConfig = getDefaultConfig(__dirname);
 
 /**
  * Metro configuration
  * https://reactnative.dev/docs/metro
  *
- * @type {import('@react-native/metro-config').MetroConfig}
+ * @type {import('metro-config').MetroConfig}
  */
-const config = {};
+const config = makeMetroConfig({
+  ...defaultConfig,
+  projectRoot: __dirname,
+  watchFolders: [
+    path.resolve(__dirname, '../../node_modules'), // 추가: 루트 node_modules 포함
+  ],
+});
 
-module.exports = mergeConfig(getDefaultConfig(__dirname), config);
+module.exports = withNativeWind(config, {
+  input: './src/theme/globals.css',
+});
