f0aa44823a29b38c5d0512c4a92a4411d3f9e840
SouthNova <jsn950509@gmail.com>
2025-04-22 21:41:59 +0900

    refactor: Tailwind CSS 파일 추가, App 디렉토리 변경 및 globals.css import

---
+++ apps/mobile/src/App.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import { Text, View } from 'react-native';
+
+import './theme/globals.css';
+
+export default function App(): React.JSX.Element {
+  return (
+    <View>
+      <Text className=''>Hello World</Text>
+    </View>
+  );
+}
